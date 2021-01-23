949f5d292859bbd7dd1f515e2672c330f63cfd27
suporte <suporte@STFBHZC044043-L>
2021-01-23 17:29:45 -0300

    Criado projeto contendo estrutura inicial para leitura de imagem.

---
+++ ScanText/ScanText.Engine.Tesseract/Entities/TesseractEngineOCR.cs
@@ -0,0 +1,13 @@
+﻿using ScanText.Engine.Tesseract.Interfaces;
+using System.Threading.Tasks;
+
+namespace ScanText.Engine.Tesseract.Entities
+{
+    public class TesseractEngineOCR : ITesseractEngineOCR
+    {
+        public async Task<string> ReadImageAsync(string base64)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ ScanText/ScanText.Engine.Tesseract/Interfaces/ITesseractEngineOCR.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace ScanText.Engine.Tesseract.Interfaces
+{
+    public interface ITesseractEngineOCR
+    {
+        Task<string> ReadImageAsync(string base64);
+    }
+}
