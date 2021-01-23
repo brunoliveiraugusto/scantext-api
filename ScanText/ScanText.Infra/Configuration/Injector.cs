5ba2c22220491be4f0179201bb95c09eeecaba2c
suporte <suporte@STFBHZC044043-L>
2021-01-23 18:28:35 -0300

    Criado projeto Infra para configuração do projeto.

---
+++ ScanText/ScanText.Infra/Configuration/Injector.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ScanText.Application.Interfaces;
+using ScanText.Application.Services;
+
+namespace ScanText.Api.Configurations
+{
+    public class Injector
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Service
+            services.AddScoped<IScanAppService, ScanAppService>();
+
+            //Engine
+            services.AddScoped<IScanAppService, ScanAppService>();
+        }
+    }
+}
