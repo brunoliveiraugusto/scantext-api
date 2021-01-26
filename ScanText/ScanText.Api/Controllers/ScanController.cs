125e0537905f8332d1400cafb0d2b2d3dda4d728
suporte <suporte@STFBHZC044043-L>
2021-01-25 23:18:01 -0300

    Ajustes de parâmetro e tipagem do método.

--- ScanText/ScanText.Api/Controllers/ScanController.cs
+++ ScanText/ScanText.Api/Controllers/ScanController.cs
@@ -18,11 +18,11 @@ namespace ScanText.Api.Controllers
         /// <summary>
         /// API responsável por ler uma imagem e retornar o texto contido na mesma.
         /// </summary>
-        /// <response code="200">Indica que a leitura da imagem foi realizada com sucesso.</response>
-        [HttpGet("{base64}")]
-        public async Task<IActionResult> LerTextoImagem(string base64)
+        /// <response code="200">Sucesso.</response>
+        [HttpGet("{urlImg}")]
+        public IActionResult LerTextoImagem(string urlImg)
         {
-            var response = await _scanAppService.LerTextoImagemAsync(base64);
+            var response = _scanAppService.LerTextoImagem(urlImg);
             return Ok(response);
         }
     }
