980166fb88f3906ce349dc6f4becd888952f1819
suporte <suporte@STFBHZC044043-L>
2021-01-26 22:16:14 -0300

    Movimentação de classes de configuração para o projeto Data.

--- ScanText/ScanText.Infra/Configuration/DataBase/Interface/IScanTextDatabaseSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ScanText.Infra.Configuration.DataBase.Interface
-{
-    public interface IScanTextDatabaseSettings
-    {
-        string ConnectionString { get; set; }
-        string DatabaseName { get; set; }
-    }
-}
