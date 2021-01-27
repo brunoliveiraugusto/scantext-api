8065bc219adf7d2eb26faeb415f085627f77ffe5
suporte <suporte@STFBHZC044043-L>
2021-01-26 22:14:43 -0300

    Criada classe de configuração de acesso ao banco de dados.

---
+++ ScanText/ScanText.Data/Database/Interface/IScanTextDatabaseSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace ScanText.Infra.Configuration.DataBase.Interface
+{
+    public interface IScanTextDatabaseSettings
+    {
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
