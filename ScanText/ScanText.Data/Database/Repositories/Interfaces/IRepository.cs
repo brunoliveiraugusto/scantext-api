64fffd3899c06376352225e29120f56c09431f95
suporte <suporte@STFBHZC044043-L>
2021-01-27 19:48:08 -0300

    Criada classe base Repository e implementado métodos CRUD parcialmente.

---
+++ ScanText/ScanText.Data/Database/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ScanText.Data.Database.Repositories
+{
+    public interface IRepository<TEntity>
+    {
+        Task<TEntity> InserirAsync(TEntity entity);
+        void Remover(Guid id);
+        Task<TEntity> ObterPorIdAsync(Guid id);
+        TEntity Atualizar(TEntity entity);
+    }
+}
---
+++ ScanText/ScanText.Data/Database/Repositories/Repository.cs
@@ -0,0 +1,42 @@
+﻿using MongoDB.Driver;
+using ScanText.Infra.Configuration.Database.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScanText.Data.Database.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity>
+    {
+        protected readonly ScanTextMongoContext _context;
+        protected IMongoCollection<TEntity> DbSet;
+
+        public Repository(ScanTextMongoContext context)
+        {
+            _context = context;
+            DbSet = _context.GetCollection<TEntity>();
+        }
+
+        public virtual TEntity Atualizar(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual async Task<TEntity> InserirAsync(TEntity entity)
+        {
+            await DbSet.InsertOneAsync(entity);
+            return entity;
+        }
+
+        public virtual Task<TEntity> ObterPorIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Remover(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
