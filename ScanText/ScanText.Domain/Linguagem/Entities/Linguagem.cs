42a7ab39f3cca3b7741c33a0b5674e034df343d5
suporte <suporte@STFBHZC044043-L>
2021-01-26 21:36:15 -0300

    Criada entidade Linguagem.

---
+++ ScanText/ScanText.Domain/Linguagem/Entities/Linguagem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ScanText.Domain.Linguagem.Entities
+{
+    public class Linguagem
+    {
+        public Guid Id { get; set; }
+        public string Sigla { get; set; }
+        public string Idioma { get; set; }
+    }
+}
