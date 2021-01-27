46ef02a82fa9b394dcf9f41e26da60fe55bd5599
suporte <suporte@STFBHZC044043-L>
2021-01-27 19:45:59 -0300

    Criado service e método para cadastrar linguagem.

---
+++ ScanText/ScanText.Application/Interfaces/ILinguagemAppService.cs
@@ -0,0 +1,10 @@
+﻿using ScanText.Domain.Linguagem.Entities;
+using System.Threading.Tasks;
+
+namespace ScanText.Application.Interfaces
+{
+    public interface ILinguagemAppService
+    {
+        Task InserirLinguagemAsync(Linguagem linguagem);
+    }
+}
---
+++ ScanText/ScanText.Application/Services/LinguagemAppService.cs
@@ -0,0 +1,22 @@
+﻿using ScanText.Application.Interfaces;
+using ScanText.Data.Database.Repositories.Interfaces;
+using ScanText.Domain.Linguagem.Entities;
+using System.Threading.Tasks;
+
+namespace ScanText.Application.Services
+{
+    public class LinguagemAppService : ILinguagemAppService
+    {
+        private readonly ILinguagemRepository _linguagemRepository;
+
+        public LinguagemAppService(ILinguagemRepository linguagemRepository)
+        {
+            _linguagemRepository = linguagemRepository;
+        }
+
+        public async Task InserirLinguagemAsync(Linguagem linguagem)
+        {
+            await _linguagemRepository.InserirAsync(linguagem);
+        }
+    }
+}
