095e72c454a75b659adfecacc4235980ffcea060
suporte <suporte@STFBHZC044043-L>
2021-01-26 22:18:11 -0300

    Criada classe LinguagemRepository para acesso a collection de Linguagem.

---
+++ ScanText/ScanText.Data/Database/Repositories/LinguagemRepository.cs
@@ -0,0 +1,14 @@
+﻿using ScanText.Infra.Configuration.Database.Context;
+
+namespace ScanText.Domain.Linguagem.Repository
+{
+    public class LinguagemRepository
+    {
+        private readonly ScanTextMongoContext _context;
+
+        public LinguagemRepository(ScanTextMongoContext context)
+        {
+            _context = context;
+        }
+    }
+}
