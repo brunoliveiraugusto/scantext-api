793ae12b7c2340e198fa0eb5b95ab858d999a601
suporte <suporte@STFBHZC044043-L>
2021-01-27 20:14:10 -0300

    Criada classe para transformar modelos VM em entidades.

---
+++ ScanText/ScanText.Application/AutoMapper/ViewModelToEntitie.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using ScanText.Application.ViewModels;
+using ScanText.Domain.Linguagem.Entities;
+
+namespace ScanText.Application.AutoMapper
+{
+    public class ViewModelToEntitie : Profile
+    {
+        public ViewModelToEntitie()
+        {
+            CreateMap<LinguagemViewModel, Linguagem>()
+                .ForMember(dest => dest.Idioma, opt => opt.MapFrom(x => x.Idioma))
+                .ForMember(dest => dest.Sigla, opt => opt.MapFrom(x => x.Sigla))
+                .ForMember(dest => dest.Id, opt => opt.Ignore());
+        }
+    }
+}
