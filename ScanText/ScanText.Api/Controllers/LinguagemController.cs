bccbcec76b2464ae64ce534f5d6636f3db49a779
suporte <suporte@STFBHZC044043-L>
2021-01-27 20:20:53 -0300

    Criada controller para Linguagem e api para criar uma nova linguagem.

---
+++ ScanText/ScanText.Api/Controllers/LinguagemController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScanText.Application.Interfaces;
+using ScanText.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScanText.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LinguagemController : ControllerBase
+    {
+        private readonly ILinguagemAppService _linguagemAppService;
+
+        public LinguagemController(ILinguagemAppService linguagemAppService)
+        {
+            _linguagemAppService = linguagemAppService;
+        }
+
+        /// <summary>
+        /// API responsável por criar uma nova linguagem.
+        /// </summary>
+        /// <response code="200">Sucesso.</response>
+        [HttpPost()]
+        public async Task Criar([FromBody] LinguagemViewModel linguagemViewModel)
+        {
+            await _linguagemAppService.InserirLinguagemAsync(linguagemViewModel);
+        }
+    }
+}
