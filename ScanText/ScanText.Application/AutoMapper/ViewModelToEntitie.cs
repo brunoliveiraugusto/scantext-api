2926d7b42f952991ce62d14d98344d626c1d7b40
suporte <suporte@STFBHZC044043-L>
2021-02-06 01:11:31 -0300

    Adicionada propriedade Id ao mapper.

--- ScanText/ScanText.Application/AutoMapper/ViewModelToEntitie.cs
+++ ScanText/ScanText.Application/AutoMapper/ViewModelToEntitie.cs
@@ -9,6 +9,7 @@ namespace ScanText.Application.AutoMapper
         public ViewModelToEntitie()
         {
             CreateMap<LinguagemViewModel, Linguagem>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(x => x.Id))
                 .ForMember(dest => dest.Idioma, opt => opt.MapFrom(x => x.Idioma))
                 .ForMember(dest => dest.Sigla, opt => opt.MapFrom(x => x.Sigla));
         }
