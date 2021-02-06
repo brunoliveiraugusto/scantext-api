29902f8c20ff2ba453eca50a7c7d1f43641c3c35
suporte <suporte@STFBHZC044043-L>
2021-02-06 01:12:30 -0300

    Criado view model da imagem.

---
+++ ScanText/ScanText.Application/ViewModels/ImagemViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ScanText.Application.ViewModels
+{
+    public class ImagemViewModel
+    {
+        public string Base64 { get; set; }
+        public string Texto { get; set; }
+    }
+}
