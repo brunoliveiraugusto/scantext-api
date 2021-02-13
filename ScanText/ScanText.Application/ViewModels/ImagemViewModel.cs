70ff528d7629f8916b9f5e9b7930556b2ab1e342
suporte <suporte@STFBHZC044043-L>
2021-02-13 00:41:06 -0300

    Criação de propriedades da imagem.

--- ScanText/ScanText.Application/ViewModels/ImagemViewModel.cs
+++ ScanText/ScanText.Application/ViewModels/ImagemViewModel.cs
@@ -5,6 +5,9 @@ namespace ScanText.Application.ViewModels
     public class ImagemViewModel
     {
         public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public long Size { get; set; }
+        public string Formato { get; set; }
         public string Base64 { get; set; }
         public string Texto { get; set; }
         public LinguagemViewModel Linguagem { get; set; }
--- ScanText/ScanText.Domain/ImagemDTO/Entities/Imagem.cs
+++ ScanText/ScanText.Domain/ImagemDTO/Entities/Imagem.cs
@@ -4,6 +4,9 @@ namespace ScanText.Domain.Linguagem.Entities
 {
     public class Imagem : Entity<Imagem>
     {
+        public string Nome { get; set; }
+        public long Size { get; set; }
+        public string Formato { get; set; }
         public string Base64 { get; set; }
         public string Texto { get; set; }
         public Linguagem Linguagem { get; set; }
