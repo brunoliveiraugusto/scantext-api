8758c0d0e5996681caa662d16360da0c62d6dfb9
suporte <suporte@STFBHZC044043-L>
2021-02-13 14:20:00 -0300

    Criada classe de serviço para a imagem e implementada interface IImagemAppService.

---
+++ ScanText/ScanText.Application/Interfaces/IImagemAppService.cs
@@ -0,0 +1,10 @@
+﻿using ScanText.Application.ViewModels;
+using ScanText.Domain.Linguagem.Entities;
+
+namespace ScanText.Application.Interfaces
+{
+    public interface IImagemAppService : IServiceApp<ImagemViewModel>
+    {
+        Imagem ImagemViewModelToImagem(ImagemViewModel imagemViewModel);
+    }
+}
---
+++ ScanText/ScanText.Application/Services/ImagemAppService.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using ScanText.Application.Interfaces;
+using ScanText.Application.ViewModels;
+using ScanText.Data.Database.Repositories.Interfaces;
+using ScanText.Domain.Linguagem.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ScanText.Application.Services
+{
+    public class ImagemAppService : IImagemAppService
+    {
+        private readonly IImagemRepository _imagemRepository;
+        private readonly IMapper _mapper;
+
+        public ImagemAppService(IImagemRepository imagemRepository, IMapper mapper)
+        {
+            _imagemRepository = imagemRepository;
+            _mapper = mapper;
+        }
+
+        public async Task AtualizarAsync(ImagemViewModel imagemViewModel)
+        {
+            var imagem = ImagemViewModelToImagem(imagemViewModel);
+            await _imagemRepository.AtualizarAsync(imagem);
+        }
+
+        public async Task InserirAsync(ImagemViewModel imagemViewModel)
+        {
+            var imagem = ImagemViewModelToImagem(imagemViewModel);
+            await _imagemRepository.InserirAsync(imagem);
+        }
+
+        public async Task<ImagemViewModel> ObterPorIdAsync(Guid id)
+        {
+            var imagem = await _imagemRepository.ObterPorIdAsync(id);
+            return _mapper.Map<ImagemViewModel>(imagem);
+        }
+
+        public async Task<IEnumerable<ImagemViewModel>> ObterTodosAsync()
+        {
+            var imagens = await _imagemRepository.ObterTodosAsync();
+            return _mapper.Map<IEnumerable<ImagemViewModel>>(imagens);
+        }
+
+        public async Task RemoverAsync(Guid id)
+        {
+            await _imagemRepository.RemoverAsync(id);
+        }
+
+        public Imagem ImagemViewModelToImagem(ImagemViewModel imagemViewModel)
+        {
+            return _mapper.Map<Imagem>(imagemViewModel);
+        }
+    }
+}
