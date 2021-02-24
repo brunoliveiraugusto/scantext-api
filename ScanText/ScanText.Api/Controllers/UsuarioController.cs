b47a52f392addaacd62737f231cd2ed4c3bab9f1
suporte <suporte@STFBHZC044043-L>
2021-02-24 20:15:00 -0300

    Criada controller para o usuário com método post.

---
+++ ScanText/ScanText.Api/Controllers/UsuarioController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScanText.Application.Interfaces;
+using ScanText.Application.ViewModels;
+using System.Threading.Tasks;
+
+namespace ScanText.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioAppService _usuarioAppService;
+
+        public UsuarioController(IUsuarioAppService usuarioAppService)
+        {
+            _usuarioAppService = usuarioAppService;
+        }
+
+        /// <summary>
+        /// API responsável por criar um novo usuário
+        /// </summary>
+        /// <response code="200">Sucesso.</response>
+        [HttpPost()]
+        public async Task<IActionResult> Criar([FromBody] UsuarioViewModel usuarioViewModel)
+        {
+            await _usuarioAppService.InserirAsync(usuarioViewModel);
+            return Ok();
+        }
+    }
+}
