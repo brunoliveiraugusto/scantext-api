6fc39d5462e1c34c6b5035cc3ae5d96b580eceaf
suporte <suporte@STFBHZC044043-L>
2021-02-24 20:15:27 -0300

    Criado service para o usuário e implementado a interface IUsuarioAppService.

---
+++ ScanText/ScanText.Application/Interfaces/IUsuarioAppService.cs
@@ -0,0 +1,11 @@
+﻿using ScanText.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScanText.Application.Interfaces
+{
+    public interface IUsuarioAppService : IServiceApp<UsuarioViewModel>
+    {
+    }
+}
---
+++ ScanText/ScanText.Application/Services/UsuarioAppService.cs
@@ -0,0 +1,44 @@
+﻿using ScanText.Application.Interfaces;
+using ScanText.Application.ViewModels;
+using ScanText.Data.Database.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ScanText.Application.Services
+{
+    public class UsuarioAppService : IUsuarioAppService
+    {
+        private readonly IUsuarioRepository _usuarioRepository;
+
+        public UsuarioAppService(IUsuarioRepository usuarioRepository)
+        {
+            _usuarioRepository = usuarioRepository;
+        }
+
+        public Task AtualizarAsync(UsuarioViewModel model, Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InserirAsync(UsuarioViewModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UsuarioViewModel> ObterPorIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<UsuarioViewModel>> ObterTodosAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoverAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
