d71d4455c8db28b60832970eac66eee9467bb2b0
suporte <suporte@STFBHZC044043-L>
2021-02-24 20:54:25 -0300

    Criada view model para o usuário.

--- ScanText/ScanText.Application/ViewModels/UsuarioViewModel.cs
+++ ScanText/ScanText.Application/ViewModels/UsuarioViewModel.cs
@@ -6,5 +6,9 @@ namespace ScanText.Application.ViewModels
 {
     public class UsuarioViewModel
     {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
     }
 }
