a467a277ce71ee6244f2e980923c5ae7fd222955
suporte <suporte@STFBHZC044043-L>
2021-02-24 20:53:01 -0300

    Criado projeto Security e método para criptografar senha.

---
+++ ScanText/ScanText.Security/Encrypt/EncryptData.cs
@@ -0,0 +1,17 @@
+﻿using ScanText.Security.Encrypt.Interfaces;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ScanText.Security.Encrypt
+{
+    public class EncryptData : IEncryptData
+    {
+        public string Encrypt(string password)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(password);
+            data = new SHA256Managed().ComputeHash(data);
+            string hash = Encoding.ASCII.GetString(data);
+            return hash;
+        }
+    }
+}
---
+++ ScanText/ScanText.Security/Encrypt/Interfaces/IEncryptData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScanText.Security.Encrypt.Interfaces
+{
+    public interface IEncryptData
+    {
+        public string Encrypt(string password);
+    }
+}
