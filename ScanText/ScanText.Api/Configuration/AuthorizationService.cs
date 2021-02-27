7f992c916470465caa628c252cd4a0e896844f42
suporte <suporte@STFBHZC044043-L>
2021-02-27 16:36:50 -0300

    Criada classe para perfis de autorização ao consumo das apis.

---
+++ ScanText/ScanText.Api/Configuration/AuthorizationService.cs
@@ -0,0 +1,8 @@
+﻿namespace ScanText.Api.Configuration
+{
+    internal static class AuthorizationService
+    {
+        internal const string Administrador = "administrador";
+        internal const string UsuarioComum = "usuarioComum";
+    }
+}
