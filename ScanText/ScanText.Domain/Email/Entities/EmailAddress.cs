93ceae958bd8268545fe4c853bef2fad635d5ed2
suporte <suporte@STFBHZC044043-L>
2021-03-29 20:20:13 -0300

    Criada entidade EmailAddress.

---
+++ ScanText/ScanText.Domain/Email/Entities/EmailAddress.cs
@@ -0,0 +1,25 @@
+﻿namespace ScanText.Domain.Email.Entities
+{
+    public class EmailAddress
+    {
+        public EmailAddress()
+        {
+
+        }
+
+        public EmailAddress(string name, string email, string subject, string plainTextContent, string htmlContent)
+        {
+            Name = name;
+            Email = email;
+            Subject = subject;
+            PlainTextContent = plainTextContent;
+            HtmlContent = htmlContent;
+        }
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string PlainTextContent { get; set; }
+        public string HtmlContent { get; set; }
+    }
+}
