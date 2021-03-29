6da380b8ff20b8439071dbfa852caa0c85061641
suporte <suporte@STFBHZC044043-L>
2021-03-29 20:18:44 -0300

    Criada classe de configuração do send grid.

---
+++ ScanText/ScanText.Data/Database/Settings/SendGridSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScanText.Data.Settings
+{
+    public class SendGridSettings
+    {
+        public string Key { get; set; }
+        public string To { get; set; }
+        public string Name { get; set; }
+    }
+}
