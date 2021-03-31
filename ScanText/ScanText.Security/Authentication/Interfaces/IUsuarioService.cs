9c88298cb17b4632aae8dcc23aabc722fa9fc97f
suporte <suporte@STFBHZC044043-L>
2021-03-31 20:24:09 -0300

    Criada classe de usuário para obtenção de dados do usuário logado.

---
+++ ScanText/ScanText.Security/Authentication/Interfaces/IUsuarioService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ScanText.Security.Authentication.Interfaces
+{
+    public interface IUsuarioService
+    {
+        Guid GetUserId();
+        bool IsAuthenticated();
+    }
+}
---
+++ ScanText/ScanText.Security/Authentication/Services/UsuarioService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using ScanText.Security.Authentication.Extensions;
+using ScanText.Security.Authentication.Interfaces;
+using System;
+
+namespace ScanText.Security.Authentication.Services
+{
+    public class UsuarioService : IUsuarioService
+    {
+        private readonly IHttpContextAccessor _acessor;
+
+        public UsuarioService(IHttpContextAccessor acessor)
+        {
+            _acessor = acessor;
+        }
+
+        public Guid GetUserId()
+        {
+            return IsAuthenticated() ? Guid.Parse(_acessor.HttpContext.User.GetUserId()) : throw new ArgumentException("Usuário não autenticado.");
+        }
+
+        public bool IsAuthenticated()
+        {
+            return _acessor.HttpContext.User.Identity.IsAuthenticated;
+        }
+    }
+}
