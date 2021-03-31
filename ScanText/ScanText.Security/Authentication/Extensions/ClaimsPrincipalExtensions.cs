a057056d74fafdc185f6c7001d9c98ff3353f8e4
suporte <suporte@STFBHZC044043-L>
2021-03-31 20:24:39 -0300

    Criada classe de estática com método para obter id do usuário logado.

---
+++ ScanText/ScanText.Security/Authentication/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Claims;
+
+namespace ScanText.Security.Authentication.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string GetUserId(this ClaimsPrincipal claimsPrincipal)
+        {
+            if(claimsPrincipal == null)
+            {
+                throw new ArgumentException("Usuário não autenticado.");
+            }
+
+            var claim = claimsPrincipal.FindFirst(ClaimTypes.NameIdentifier);
+            return claim?.Value;
+        }
+    }
+}
