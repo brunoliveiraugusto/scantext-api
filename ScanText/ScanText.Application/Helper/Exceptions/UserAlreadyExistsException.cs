3bfe24c9ecdef06e936b43c6307b1f84c74cf2e9
suporte <suporte@STFBHZC044043-L>
2021-04-05 20:54:20 -0300

    Criada exceção para usuário já cadastrado.

---
+++ ScanText/ScanText.Application/Helper/Exceptions/UserAlreadyExistsException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ScanText.Application.Helper.Exceptions
+{
+    public class UserAlreadyExistsException : Exception
+    {
+        public UserAlreadyExistsException() : base("Usuário já cadastrado.")
+        {
+
+        }
+    }
+}
