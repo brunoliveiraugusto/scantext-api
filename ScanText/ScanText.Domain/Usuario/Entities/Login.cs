a968949e72682cef5c83cac2a06f7098b58b092e
suporte <suporte@STFBHZC044043-L>
2021-04-07 20:39:44 -0300

    Adicionada herança.

--- ScanText/ScanText.Domain/Usuario/Entities/Login.cs
+++ ScanText/ScanText.Domain/Usuario/Entities/Login.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using ScanText.Domain.DomainObjects;
+using System;
 
 namespace ScanText.Domain.Usuario.Entities
 {
-    public class Login
+    public class Login : Entity<Login>
     {
-        public Guid Id { get; set; }
         public string Username { get; set; }
         public string NomeCompleto { get; set; }
         public string Token { get; set; }
