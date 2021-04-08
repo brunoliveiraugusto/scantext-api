7aa192b6b28658723c1a653eda7eabb270425a87
suporte <suporte@STFBHZC044043-L>
2021-04-08 10:49:09 -0300

    Criação de viewmodel para o perfil.

---
+++ ScanText/ScanText.Application/ViewModels/PerfilViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ScanText.Application.ViewModels
+{
+    public class PerfilViewModel
+    {
+        public Guid Id { get; set; }
+        public string Sigla { get; set; }
+        public string Descricao { get; set; }
+        public bool Ativo { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
