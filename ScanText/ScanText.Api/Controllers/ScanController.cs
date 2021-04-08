d797b0ee994f2daa6dc0b9ff18e136a1a6b41387
suporte <suporte@STFBHZC044043-L>
2021-04-08 12:26:53 -0300

    Criado endpoint para obtenção de qrcode.

--- ScanText/ScanText.Api/Controllers/ScanController.cs
+++ ScanText/ScanText.Api/Controllers/ScanController.cs
@@ -20,12 +20,25 @@ namespace ScanText.Api.Controllers
         /// <summary>
         /// API responsável por ler uma imagem e retornar o texto contido na mesma.
         /// </summary>
-        /// <response code="200">Sucesso.</response>
+        /// <response code="200">Imagem Processada.</response>
         [HttpPost()]
         [Authorize(Roles = AuthorizationService.Todos)]
         public IActionResult LerTextoImagem([FromBody] ImagemViewModel imagemVM)
         {
             var response = _scanAppService.LerTextoImagem(imagemVM);
+            return Ok(response);
+        }
+
+        /// <summary>
+        /// API responsável por gerar um QrCode.
+        /// </summary>
+        /// <response code="200">QrCode gerado.</response>
+        [HttpGet()]
+        [Route("obter-qr-code/{text}")]
+        [Authorize(Roles = AuthorizationService.Todos)]
+        public IActionResult ObterQrCodeImagem([FromQuery] string text)
+        {
+            var response = _scanAppService.ObterQrCodeImagem(text);
             return Ok(response);
         }
     }
