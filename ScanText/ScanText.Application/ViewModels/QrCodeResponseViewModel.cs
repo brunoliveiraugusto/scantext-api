874a6448b93a5b4c528ed114efc812fdcbb71433
suporte <suporte@STFBHZC044043-L>
2021-04-08 19:20:54 -0300

    Criação de view model response e ajuste de propriedade.

---
+++ ScanText/ScanText.Application/ViewModels/QrCodeResponseViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScanText.Application.ViewModels
+{
+    public class QrCodeResponseViewModel
+    {
+        public string Code { get; set; }
+    }
+}
--- ScanText/ScanText.Application/ViewModels/QrCodeViewModel.cs
+++ ScanText/ScanText.Application/ViewModels/QrCodeViewModel.cs
@@ -2,6 +2,6 @@
 {
     public class QrCodeViewModel
     {
-        public byte[] Code { get; set; }
+        public string Text { get; set; }
     }
 }
