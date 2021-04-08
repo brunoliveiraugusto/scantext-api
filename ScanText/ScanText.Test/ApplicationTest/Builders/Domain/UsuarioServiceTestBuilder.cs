832c0e75afd4ccb198af0bbec89daf4b28db871c
suporte <suporte@STFBHZC044043-L>
2021-04-07 21:19:47 -0300

    Criado mock para usuário.

---
+++ ScanText/ScanText.Test/ApplicationTest/Builders/Domain/UsuarioServiceTestBuilder.cs
@@ -0,0 +1,13 @@
+﻿using Moq;
+using ScanText.Security.Authentication.Interfaces;
+
+namespace ScanText.Test.ApplicationTest.Builders.Domain
+{
+    public class UsuarioServiceTestBuilder : BaseTestBuilder<Mock<IUsuarioService>>
+    {
+        public UsuarioServiceTestBuilder()
+        {
+            Model = new Mock<IUsuarioService>();
+        }
+    }
+}
