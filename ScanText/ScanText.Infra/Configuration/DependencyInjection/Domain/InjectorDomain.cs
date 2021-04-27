d163a18c8f30875440ae351f70181465db594ed7
suporte <suporte@STFBHZC044043-L>
2021-04-26 21:15:05 -0300

    Injeção de abstração.

--- ScanText/ScanText.Infra/Configuration/DependencyInjection/Domain/InjectorDomain.cs
+++ ScanText/ScanText.Infra/Configuration/DependencyInjection/Domain/InjectorDomain.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using ScanText.Domain.Email.Entities;
 using ScanText.Domain.Email.Entities.Interfaces;
+using ScanText.Domain.Utils.Interfaces;
+using ScanText.Domain.Utils.Notification;
 
 namespace ScanText.Infra.Configuration.DependencyInjection.Domain
 {
@@ -9,6 +11,7 @@ namespace ScanText.Infra.Configuration.DependencyInjection.Domain
         public static void InjectDomain(this IServiceCollection services)
         {
             services.AddSingleton<IEmailAddress, EmailAddress>();
+            services.AddSingleton<INotificationService, NotificationService>();
         }
     }
 }
