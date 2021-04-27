39e8290e390d4238621c007f90941677e378fe31
suporte <suporte@STFBHZC044043-L>
2021-04-26 22:22:37 -0300

    Criação de builder para o notification service.

---
+++ ScanText/ScanText.Test/ApplicationTest/Builders/Utils/Notification/NotificationServiceTestBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Moq;
+using ScanText.Domain.Utils.Interfaces;
+using ScanText.Domain.Utils.Notification;
+
+namespace ScanText.Test.ApplicationTest.Builders.Utils.Notification
+{
+    public class NotificationServiceTestBuilder : BaseTestBuilder<NotificationService>
+    {
+        public NotificationServiceTestBuilder()
+        {
+            Model = new NotificationService();
+        }
+    }
+}
