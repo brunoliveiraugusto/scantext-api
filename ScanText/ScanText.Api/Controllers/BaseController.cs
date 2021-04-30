d4079ec8f7f75ec61601e143105a788402186548
suporte <suporte@STFBHZC044043-L>
2021-04-29 22:00:37 -0300

    Criação de classe base controller.

---
+++ ScanText/ScanText.Api/Controllers/BaseController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScanText.Domain.Utils.Interfaces;
+using System.Linq;
+
+namespace ScanText.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseController : ControllerBase
+    {
+        private readonly INotificationService _notificationService;
+
+        public BaseController(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        protected IActionResult ResponseRequest(object result = null)
+        {
+            if(ValidRequest())
+            {
+                return Ok(new
+                {
+                    sucess = true,
+                    data = result
+                });
+            }
+
+            return BadRequest(new
+            {
+                success = false,
+                errors = _notificationService.Notifications.Select(notification => new { notification.Chave, notification.Valor })
+            });
+        }
+
+        protected bool ValidRequest()
+        {
+            return !_notificationService.Notifications.Any();
+        }
+    }
+}
