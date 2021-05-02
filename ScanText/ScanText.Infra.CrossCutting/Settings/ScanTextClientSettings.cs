43896329b6fc3e2dc1ebf136e0b6a649b2a3910b
suporte <suporte@STFBHZC044043-L>
2021-05-02 12:57:20 -0300

    Criação de projeto CrossCutting e classe para configuração do cliente.

---
+++ ScanText/ScanText.Infra.CrossCutting/Settings/ScanTextClientSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScanText.Infra.CrossCutting.Settings
+{
+    public class ScanTextClientSettings
+    {
+        public string Url { get; set; }
+    }
+}
