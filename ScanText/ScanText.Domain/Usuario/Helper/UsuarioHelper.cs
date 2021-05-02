7d8595e634706376c41294449f4c6188bdf118ec
suporte <suporte@STFBHZC044043-L>
2021-05-01 21:04:47 -0300

    Criação de método para aplicação de máscara de e-mail.

---
+++ ScanText/ScanText.Domain/Usuario/Helper/UsuarioHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScanText.Domain.Usuario.Helper
+{
+    public class UsuarioHelper
+    {
+        public static string AplicarMascaraEmail(string email)
+        {
+            char[] caracteresEmail = email.ToCharArray();
+            string emailComMascara = string.Empty;
+            bool dominio = false;
+
+            for (int i = 0; i < caracteresEmail.Length; i++)
+            {
+                if (caracteresEmail[i] == '@')
+                    dominio = true;
+
+                if (i == 0 || i == 1 || i == caracteresEmail.Length - 1 || i == caracteresEmail.Length - 2 || i == caracteresEmail.Length - 3 || dominio)
+                {
+                    emailComMascara += caracteresEmail[i];
+                }
+                else
+                {
+                    emailComMascara += "*";
+                }
+            }
+
+            return emailComMascara;
+        }
+    }
+}
