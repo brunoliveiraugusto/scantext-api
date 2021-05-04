fa87d219189f8863f85a9d977edb5708e2d48355
suporte <suporte@STFBHZC044043-L>
2021-05-03 21:00:34 -0300

    Criação de view model atualiza senha.

---
+++ ScanText/ScanText.Application/ViewModels/AtualizaSenhaViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ScanText.Application.ViewModels
+{
+    public class AtualizaSenhaViewModel
+    {
+        public string Username { get; set; }
+        public string Token { get; set; }
+        public string Senha { get; set; }
+        public string ConfirmaSenha { get; set; }
+    }
+}
